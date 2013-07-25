9a3fe72e0c41098620babea487f89213d0f105d9
DAVe3283 <DAVe3283@gmail.com>
2013-07-24 21:50:59 -0600

    Add support for getting movie height/width/quality. This makes things terribly slow. Also, it is really quick/dirty, and probably has memory leaks, etc.

--- MovieInfo/Program.cs
+++ MovieInfo/Program.cs
@@ -2,13 +2,15 @@
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using DirectShowLib;
 
 namespace MovieInfo
 {
     /// <summary>
     /// A Move object to hold details on the move
     /// </summary>
-    public struct Movie
+    public class Movie
     {
         /// <summary>
         /// The (nice) name of the movie
@@ -65,33 +67,84 @@ namespace MovieInfo
                 return File.LastWriteTime;
             }
         }
+
+        /// <summary>
+        /// The width of the video frame (if it can be determined)
+        /// </summary>
+        public int Width = 0;
+
+        /// <summary>
+        /// The height of the video frame (if it can be determined)
+        /// </summary>
+        public int Height = 0;
+
+        public VideoQuality Quality
+        {
+            get
+            {
+                if ((Width == 0) || (Height == 0))
+                {
+                    return VideoQuality.Unknown;
+                }
+                else if (Width > 1888)
+                {
+                    return VideoQuality.HD_1080;
+                }
+                else if (Width > 1248)
+                {
+                    return VideoQuality.HD_720;
+                }
+                else
+                {
+                    double aspectRatio = (double)Width / (double)Height;
+                    if (aspectRatio > 1.6)                                      // 16:10 or wider
+                    {
+                        return VideoQuality.SD_Widescreen;
+                    }
+                    else
+                    {
+                        return VideoQuality.SD_Fullscreen;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// The quality of a video
+        /// </summary>
+        public enum VideoQuality
+        {
+            /// <summary>
+            /// Unknown quality (could not get dimensions?)
+            /// </summary>
+            Unknown,
+
+            /// <summary>
+            /// Standard Definition (DVD), Fullscreen
+            /// </summary>
+            SD_Fullscreen,
+
+            /// <summary>
+            /// Standard Definition (DVD), Widescreen
+            /// </summary>
+            SD_Widescreen,
+
+            /// <summary>
+            /// High Definition (Blu-Ray), 720p/i
+            /// </summary>
+            HD_720,
+
+            /// <summary>
+            /// High Definition (Blu-Ray), 1080p/i
+            /// </summary>
+            HD_1080,
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            // Move movies to subdirectories
-            //// Match things like "I'm A Moive! (2006) [PG-13] HD 1080p.mkv"
-            //Regex parser = new Regex(@"(^.+\(\d{4}\)).*\.(mkv|avi)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-            ////DirectoryInfo movieDir = new DirectoryInfo(@"\\DAVe-Server-2\Movies");
-            //DirectoryInfo movieDir = new DirectoryInfo(@"T:\DAVe-Server-2\Data\Movies");
-            //foreach (FileInfo movie in movieDir.GetFiles())
-            //{
-            //    Match movieName = parser.Match(movie.Name);
-            //    if (movieName.Success)
-            //    {
-            //        string newFolder = Path.Combine(movieDir.FullName, movieName.Groups[1].Value);
-            //        if (!Directory.Exists(newFolder))
-            //        {
-            //            Directory.CreateDirectory(newFolder);
-            //        }
-            //        string newPath = Path.Combine(newFolder, movie.Name);
-            //        movie.MoveTo(newPath);
-            //    }
-            //}
-
-
             // Match things like "I'm A Moive! (2006) [PG-13] HD 1080p.mkv"
             Regex parser = new Regex(@"(^.+\((TV\s*)?\d{4}\)).*\.(mkv|avi)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
             Regex parserBetter = new Regex(@"(^.+\((TV\s*)?\d{4}\)).*", RegexOptions.Compiled | RegexOptions.IgnoreCase);
@@ -111,7 +164,7 @@ namespace MovieInfo
             ParseDirectory(movieDir, ref parserBetter, ref ignores, ref movies);
 
             // Make a new csv
-            file.WriteLine("\"Name\",\"Path\",\"Size\",\"Added to Library\",\"Released in Theaters (may be wrong)\"");
+            file.WriteLine("\"Name\",\"Path\",\"Size\",\"Width\",\"Height\",\"Quality\",\"Added to Library\",\"Released in Theaters (may be wrong)\"");
 
             // Process the results
             System.DateTime startTime = System.DateTime.Now.AddDays(-7d);       // What movies to highlight as added recently
@@ -119,7 +172,7 @@ namespace MovieInfo
             foreach (Movie movie in movies)
             {
                 // Write the CSV line
-                string movieCSV = string.Format("\"{0}\",\"{1}\",{2},\"{3}\",\"{4}\"", movie.Name, movie.Path, movie.Size, movie.CreatedTime, movie.ModifiedTime);
+                string movieCSV = string.Format("\"{0}\",\"{1}\",{2},{3},{4},\"{5}\",\"{6}\",\"{7}\"", movie.Name, movie.Path, movie.Size, movie.Width, movie.Height, movie.Quality.ToString(), movie.CreatedTime, movie.ModifiedTime);
                 file.WriteLine(movieCSV);
 
                 // See if it is new
@@ -189,9 +242,64 @@ namespace MovieInfo
                         movie.Name.EndsWith(".avi", System.StringComparison.InvariantCultureIgnoreCase))
                     {
                         // We have a movie
-                        Movie match;
+                        Movie match = new Movie();
                         match.Name = name;
                         match.File = movie;
+
+                        // Get movie metadata
+                        IFilterGraph2 m_FilterGraph = new FilterGraph() as IFilterGraph2;
+                        ICaptureGraphBuilder2 icgb2 = new CaptureGraphBuilder2() as ICaptureGraphBuilder2;
+                        int hr;
+                        try
+                        {
+                            // Link the ICaptureGraphBuilder2 to the IFilterGraph2
+                            hr = icgb2.SetFiltergraph(m_FilterGraph);
+                            DsError.ThrowExceptionForHR(hr);
+
+                            // Add the filters necessary to render the file.  This function will
+                            // work with a number of different file types.
+                            IBaseFilter sourceFilter = null;
+                            hr = m_FilterGraph.AddSourceFilter(match.Path, match.Path, out sourceFilter);
+                            DsError.ThrowExceptionForHR(hr);
+
+                            // Get the SampleGrabber interface
+                            ISampleGrabber m_sampGrabber = (ISampleGrabber)new SampleGrabber();
+                            IBaseFilter baseGrabFlt = (IBaseFilter)m_sampGrabber;
+
+                            // Configure the Sample Grabber
+                            ConfigureSampleGrabber(m_sampGrabber);
+
+                            // Add it to the filter
+                            hr = m_FilterGraph.AddFilter(baseGrabFlt, "Ds.NET Grabber");
+                            DsError.ThrowExceptionForHR(hr);
+
+                            // Connect the pieces together, use the default renderer
+                            hr = icgb2.RenderStream(null, null, sourceFilter, baseGrabFlt, null);
+                            DsError.ThrowExceptionForHR(hr);
+
+                            // Get video size
+                            GetSize(m_sampGrabber, out match.Width, out match.Height);
+                        }
+                        catch (Exception ex)
+                        {
+                            System.Console.Error.Write("Error getting video metadata for ");
+                            System.Console.Error.WriteLine(match.Name + ":");
+                            System.Console.Error.WriteLine(ex.Message);
+                        }
+                        finally
+                        {
+                            if (icgb2 != null)
+                            {
+                                Marshal.ReleaseComObject(icgb2);
+                                icgb2 = null;
+                            }
+                            if (m_FilterGraph != null)
+                            {
+                                Marshal.ReleaseComObject(m_FilterGraph);
+                                m_FilterGraph = null;
+                            }
+                            GC.Collect();
+                        }
 
                         // Done
                         movies.Add(match);
@@ -207,6 +315,62 @@ namespace MovieInfo
             {
                 System.Console.Out.Write("Ignored ");
                 System.Console.Out.WriteLine(dir.FullName);
+            }
+        }
+
+        // Set the options on the sample grabber
+        private static void ConfigureSampleGrabber(ISampleGrabber sampGrabber)
+        {
+            AMMediaType media;
+            int hr;
+
+            // Set the media type to Video/RBG24
+            media = new AMMediaType();
+            media.majorType = MediaType.Video;
+            media.subType = MediaSubType.RGB24;
+            media.formatType = FormatType.VideoInfo;
+            hr = sampGrabber.SetMediaType(media);
+            DsError.ThrowExceptionForHR(hr);
+
+            DsUtils.FreeAMMediaType(media);
+            media = null;
+
+            // Configure the samplegrabber
+            hr = sampGrabber.SetBufferSamples(true);
+            DsError.ThrowExceptionForHR(hr);
+        }
+
+        // Get media size
+        private static void GetSize(ISampleGrabber sampGrabber, out int width, out int height)
+        {
+            width = 0;
+            height = 0;
+
+            // Get the media type from the SampleGrabber
+            AMMediaType media = new AMMediaType();
+
+            int hr = sampGrabber.GetConnectedMediaType(media);
+            DsError.ThrowExceptionForHR(hr);
+
+            try
+            {
+                if ((media.formatType != FormatType.VideoInfo) || (media.formatPtr == IntPtr.Zero))
+                {
+                    throw new NotSupportedException("Unknown Grabber Media Format");
+                }
+
+                // Get the struct
+                VideoInfoHeader videoInfoHeader = new VideoInfoHeader();
+                Marshal.PtrToStructure(media.formatPtr, videoInfoHeader);
+
+                // Grab the size info
+                width = videoInfoHeader.BmiHeader.Width;
+                height = videoInfoHeader.BmiHeader.Height;
+            }
+            finally
+            {
+                DsUtils.FreeAMMediaType(media);
+                media = null;
             }
         }
     }
