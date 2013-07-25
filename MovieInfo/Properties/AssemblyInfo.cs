85940b4eed2c47256e42f4823ca673bad32a0467
DAVe3283 <DAVe3283@gmail.com>
2013-07-24 22:11:02 -0600

    Credit myself, I guess.

--- MovieInfo/Properties/AssemblyInfo.cs
+++ MovieInfo/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MovieInfo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Get movie information for nefarious purposes")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DAVe3283")]
 [assembly: AssemblyProduct("MovieInfo")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
